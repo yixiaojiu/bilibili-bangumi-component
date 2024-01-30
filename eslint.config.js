d191f7779e05734adf2951162d6b94f0e4af4026
yixiaojiu <yi_xiao_jiu@qq.com>
2024-01-30 14:03:46 +0800

    fix: vercel api folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['**/node_modules/**', '**/dist/**', '**/loader/**', '**/.stencil/**', '**/www/**', '!packages/api/', '/api'],
+  // ignores: ['**/node_modules/**', '**/dist/**', '**/loader/**', '**/.stencil/**', '**/www/**'],
   rules: {
     'node/prefer-global/process': 'off',
     'unused-imports/no-unused-imports': 'off',
     'ts/ban-ts-comment': 'off',
+  },
+  gitignore: {
+    files: ['.eslintignore'],
   },
 })
