7b20c6fc86175ba85c57be4811043d18d0f250a3
yixiaojiu <yi_xiao_jiu@qq.com>
2024-02-22 10:23:10 +0800

    fix: :bug: encode HTML entity

--- packages/bilibili-bangumi-component/src/components/List.tsx
+++ packages/bilibili-bangumi-component/src/components/List.tsx
@@ -19,12 +19,12 @@ export const List: FunctionalComponent<ListProps> = ({ list, loading }) => {
               <div class="bbc-bangumi-item">
                 <a href={item.url} target="_blank" rel="noreferrer">
                   {/* @ts-expect-error */}
-                  <img src={item.cover} alt="cover" referrerpolicy="no-referrer" />
+                  <img src={item.cover} alt="cover" loading="lazy" referrerpolicy="no-referrer" />
                 </a>
                 <div class="bbc-bangumi-content">
                   <h3 class="bbc-bangumi-title">
-                    <a href={item.url} target="_blank" rel="noreferrer">{item.name ? item.name : item.nameCN}</a>
-                    {item.name && <small>{item.nameCN}</small>}
+                    <a href={item.url} target="_blank" rel="noreferrer" innerHTML={item.name ? item.name : item.nameCN} />
+                    {item.name && <small innerHTML={item.nameCN} />}
                   </h3>
                   <div class="bbc-bangumi-labels">
                     {item.labels.map(label => (
@@ -34,7 +34,7 @@ export const List: FunctionalComponent<ListProps> = ({ list, loading }) => {
                       </div>
                     ))}
                   </div>
-                  <p class="bbc-bangumi-summary">{item.summary}</p>
+                  <p class="bbc-bangumi-summary" innerHTML={item.summary} />
                 </div>
               </div>
               )}
